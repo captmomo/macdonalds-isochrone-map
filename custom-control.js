7b4aae6dfdd6dc6a6d2ca4e23dff0c163dc8cbb1
captmomo <wy4786@gmail.com>
2020-05-18 14:38:05 +0800

    Adding toggle for custom control

--- custom-control.js
+++ custom-control.js
@@ -6,10 +6,15 @@ toggleControl.prototype.onAdd = function(map){
     this._container = document.createElement('div');
     this._container.className = 'mapboxgl-ctrl custom';
     this._container.innerHTML = `
-    <div class='heading'><h2>Toggle travel time (mins)</h3></div>
+    <div class='heading'>
+    <p class='is-title is-3'>Toggle travel time (mins) <a style="color: #ffffff;" class="control-toggle">
+    <i class="fa fa-angle-down"></i>
+</a></div></p>
+<div class='control-content'>
     <span class='toggle-option selected' data-value='10'>10</span>
     <span class='toggle-option' data-value='20'>20</span>
-    <span class='toggle-option' data-value='30'>30</span>`
+    <span class='toggle-option' data-value='30'>30</span>
+</div>`
     return this._container;
 };
 
@@ -17,3 +22,4 @@ toggleControl.prototype.onRemove = function(){
     this._container.parentNode.removeChild(this._container);
     this._map = undefined;
 };
+
